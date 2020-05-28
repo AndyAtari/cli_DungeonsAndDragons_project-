8d351cac7d924f8c256539e1d703c23b20e5f451
Andrew Alteri <andrewsalteri@gmail.com>
2020-05-27 19:46:51 -0500

    WIP: defined methods in Cli class/ WIP on 2nd get request

--- lib/dungeon_cli/api.rb
+++ lib/dungeon_cli/api.rb
@@ -1,9 +1,9 @@
 class Api
 
-    BASE_RACES_URl = 'http://www.dnd5eapi.co/api/races'
+    BASE_RACES_URL = 'http://www.dnd5eapi.co/api/races/'
 
     def self.get_races
-       res = RestClient.get(BASE_RACES_URl)
+       res = RestClient.get(BASE_RACES_URL)
        data = JSON.parse(res.body)
        data["results"].each do |race|
         index = race["index"]
@@ -12,6 +12,13 @@ class Api
         Races.new(index, name, id)
         end
     end
+
+    def self.get_details_by_index(index)
+        res = RestClient.get("#{BASE_RACES_URL}#{index}")
+        data = JSON.parse(res.body)
+    end
 end
+
+
 
  
--- lib/dungeon_cli/cli.rb
+++ lib/dungeon_cli/cli.rb
@@ -1,3 +1,42 @@
 class Cli 
 
+    def run
+        print_welcome
+        Api.get_races
+        main
+
+    end
+
+    def main
+        print_all
+        print_selection 
+        index = user_selection 
+        valid_index?(index)
+        binding.pry
+    end
+
+    def print_welcome
+        puts "Greetings Traveler! Welcome to the Dungeons and Dragons API!"
+    end
+
+    def print_all
+        Races.all.each {|race| puts "#{race.index}"}    
+    end
+
+    def print_selection
+        puts "Please choose the desired Race for more lore:"
+    end
+
+    def print_invalid_selection
+        puts "I am sorry friend, that is an invalid Race. Please choose wisely:"
+    end
+
+    def user_selection 
+        gets.chomp 
+    end
+
+    def valid_index?(index) 
+        case index
+            
+    end
 end
